8f28b44f8d0b3d666fb4578d06e5bd5c543d8f64
Thiga <matthewthiga72@gmail.com>
2023-10-05 14:44:12 -0700

    today

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(flot));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return(0);
-}                                                                                                                                                                                                                                                    
-* Return: Always 0                                                                                             
-*/                                                                                                                                                                                                                              
-}
+}                                                                                                        
