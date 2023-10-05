c01722076e0afc8d572f356ee7ea9d05273e47b7
Thiga <matthewthiga72@gmail.com>
2023-10-05 14:25:49 -0700

    today

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - works.
+ * Return: 0
+ */
 int main(void)
 {
 	/*Function main*/
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
