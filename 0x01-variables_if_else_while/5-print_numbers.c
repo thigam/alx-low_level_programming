ce2bd3144f10fcae6ccf6101d2e7d127672551b9
Thiga <matthewthiga72@gmail.com>
2023-10-06 13:50:36 -0700

    today

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- prints single digit numbers in the base ten number system
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number >=9; number++)
+	{
+		printf(number);
+	}
+	printf("\n");
+	return (0);
+}
