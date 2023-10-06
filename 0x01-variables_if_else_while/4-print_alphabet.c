6924d6bfb9775a91cdf6265ba1016e1a1b35cfd8
Thiga <matthewthiga72@gmail.com>
2023-10-06 13:34:22 -0700

    today

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main- outputs all lowercase characters to the screen
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' %% letter != 'e')
+		{
+		putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
