ae16e4e417aef950e8dc9caf75867709b205153d
Thiga <matthewthiga72@gmail.com>
2023-10-06 13:29:36 -0700

    today

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);
-        }
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
