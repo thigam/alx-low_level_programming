73c7773ac76355f555f81b55e5545f2c76a59144
Thiga <matthewthiga72@gmail.com>
2023-10-11 12:48:49 -0700

    0x02_8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer- Prints every minute of the day
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int
+	for(n=0; n<=2; n++)
+	{
+		for(x=0; x<=9; x++)
+		{
+			for(y=0; y<6; y++)
+			{
+				for(z=0; z<=9;z++)
+				{
+                               		_putchar(n + '0');
+                                        _putchar(x + '0');
+                                    	_putchar(58);
+                                        _putchar(y + '0');
+                                     	_putchar(z + '0');
+                                }
+                        }
+                        if(x>=4 && n==2)
+			{
+				break;
+			}
+                }
+        }
+}
