4b48954bffa65160b32317ec53a725a70542f94b
Thiga <matthewthiga72@gmail.com>
2023-10-11 14:54:11 -0700

    0x02_5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,22 +2,23 @@
 
 /**
  * print_sign- prints the sign of a number
+ * @n: input integer
  * Return: 1 if greater than zero, 0 if zero and -1 if less than zero
  */
 
 int print_sign(int n)
 {
-	if(n>0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (n<0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
 	}
-	else 
+	else
 	{
 		_putchar(48);
 		return (0);
