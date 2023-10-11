043ad9a6a23c211609d7c219c0fc78609d4c6689
Thiga <matthewthiga72@gmail.com>
2023-10-11 11:18:55 -0700

    0x02_0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <_putchar.c>
+
+/**
+ * main- Prints "_putchar"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int counter;
+	char str[]="_putchar";
+	for(counter=0, counter<=len(_putchar), ++counter)
+	{
+		_putchar(str[counter];
+	}
+	Return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Practice on using nested functions
