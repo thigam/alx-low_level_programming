d02dfdae9579612e050d405542a0d76b870acc9a
Thiga <matthewthiga72@gmail.com>
2023-10-11 14:50:55 -0700

    0x02_7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,20 @@
 
 /**
  * print_last_digit- Prints the last digit of a number
+ * @z: Integer
  * Return: last digit of a number
  */
 
 int print_last_digit(int z)
 {
-	z=z%10;
+	z = z % 10;
 
-	if(z<0)
+	if (z < 0)
 	{
-		z*=-1;
+		z *= -1;
 	}
 
 	_putchar(z + '0');
 
-	return(z);
+	return (z);
 }
