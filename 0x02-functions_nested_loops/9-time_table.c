ab3dff40247a91831f0c485cfe296b94b21388b3
Thiga <matthewthiga72@gmail.com>
2023-10-11 13:05:29 -0700

    0x02_9

---
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table- Prints the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	for(n=0; n<=9; n++)
+	{
+		for(m=0; m<=9; m++)
+		{
+			x=m*n;
+			if(x>9)
+			{
+				y=x%10;
+				z=(x-y)/10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(z + '0');
+				_putchar(y + '0');
+			}
+			else
+			{
+				if(y!=0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(x + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
