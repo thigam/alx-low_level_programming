39416c433abd692235b1db58cc2a1185592a2a29
Thiga <matthewthiga72@gmail.com>
2023-10-11 13:46:00 -0700

    0x02_10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add- Adds two integers and returns the result
+ * Return: Sum of two integers
+ */
+
+int add(int x, int z)
+{
+	int sum;
+	sum = x + z;
+	return (sum);
+}
