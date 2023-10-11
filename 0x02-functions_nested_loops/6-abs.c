7d5d6a58802f1905927a59ecb489076fde70f36e
Thiga <matthewthiga72@gmail.com>
2023-10-11 14:52:22 -0700

    0x02_6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,15 @@
 
 /**
  * _abs- Gives absolute value of integer
+ * @z: Integer
  * Return: Absolute value of integer
  */
 
 int _abs(int z)
 {
-	if(z<0)
+	if (z < 0)
 	{
-		z*=-1;
+		z *= -1;
 	}
-	return(z);
+	return (z);
 }
