687f9b11c378674ce7c6b2dab0cec32411e282b1
Thiga <matthewthiga72@gmail.com>
2023-10-11 12:29:07 -0700

    0x02_7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit- Prints the last digit of a number
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int z)
+{
+	z=z%10;
+
+	if(z<0)
+	{
+		z*=-1;
+	}
+
+	_putchar(z + '0');
+
+	return(z);
+}
