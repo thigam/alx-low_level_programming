98d9f3e37cb0b4a54b985f98ebee000122cae5d1
Thiga <matthewthiga72@gmail.com>
2023-10-11 14:18:15 -0700

    0x02_100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_times_table- prints the n times table starting with 0
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	for(x=0; x<=n; x++)
+	{
+		if(n>15 || n<0)
+		{
+			break;
+		}
+
+		for(y=0; y <= n; y++)
+		{
+			z=x*y;
+			if(y != 0)
+			{
+				_putchar(44);
+				for(k=1; k<=3; k++)
+				{
+					_putchar(32);
+				}
+			}
+
+			if(z>9)
+			{
+				ones = z % 10;
+				tens = (z - ones)/10;
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+		}
+	}
+}
