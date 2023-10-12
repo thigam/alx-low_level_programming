7b7b3c3ad1bd5a0e923e3b4a5cada5801f4556e3
Thiga <matthewthiga72@gmail.com>
2023-10-12 10:38:17 -0700

    0x04_8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sqaure- Prints a square
+ * @size: Defines the size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int counter_1, counter_2;
+
+	if (n > 0)
+	{
+		for (counter_1 = 1; counter_1 <= size; counter_1++)
+		{
+			for (counter_2 = 1; counter_2 <= size; counter_2++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
