b1c2ca2b9dea57a8e4d92735d3ef51738b31a298
Thiga <matthewthiga72@gmail.com>
2023-10-12 12:54:55 -0700

    0x04_2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,16 @@
+/*
+ * File: 2-mul.c
+ * Auth: Matthew T Mwangi
+ */
+
 #include "main.h"
 
 /**
  * mul- Multiplies two integers
  * @a: One of the integers being multiplied
  * @b: The other integer being multiplied
+ *
+ * Return: The result of the multiplication
  */
 
 int mul(int a, int b)
