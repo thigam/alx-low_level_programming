e9bed523189509187dc13313cb9a7efc35bfcfae
Thiga <matthewthiga72@gmail.com>
2023-10-13 13:16:34 -0700

    0x03_3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,5 @@
 int largest_number(int a, int b, int c);
 int main(void);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
