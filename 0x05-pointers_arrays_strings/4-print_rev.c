b54e9bdeab841c0caf1bc2e2577a55b8e45cec88
Thiga <matthewthiga72@gmail.com>
2023-10-16 13:21:25 -0700

    0x05_4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev- Prints a string in reverse followed by a newline
+ * @s: Pointer to the string being printed
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int counter;
+
+	counter = 0;
+
+	while (*(s + counter) != '\0')
+	{
+		counter++
+	}
+
+	for (counter; counter >= 0; counter--)
+	{
+		_putchar(*(s + counter));
+	}
+	_putchar('\n');
+}
