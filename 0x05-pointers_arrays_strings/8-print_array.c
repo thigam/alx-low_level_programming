95b7aefaf0b45c7b993a2d187446e5e61745d6da
Thiga <matthewthiga72@gmail.com>
2023-10-16 14:00:29 -0700

    0x05_7

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array- Prints n elements of an array of integers followed by a new line
+ * @a: Pointer to array
+ * @n: number of array elements to be printed
+ * Return: nothing
+ */
+
+void print_array(int *n, int n)
+{
+	int rep_count;
+
+	for (rep_count = 0; rep_count <= n; rep_count++)
+	{
+		_putchar(n[rep_count] + '0');
+		if (rep_count != n)
+		{
+			_putchar(44);
+			_putchar(32);
+		}
+	}
+	_putchar('\n');
+}
