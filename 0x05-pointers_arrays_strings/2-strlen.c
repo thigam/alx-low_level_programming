264f4b404821a418bb7ec4afe93c2c7171de678b
Thiga <matthewthiga72@gmail.com>
2023-10-16 13:08:39 -0700

    0x05_2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen- Returns the length of a string
+ * @s: Pointer to the first character in an array
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	return (sizeof(*s));
+}
