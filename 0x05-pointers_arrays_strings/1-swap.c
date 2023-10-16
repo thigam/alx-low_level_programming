4a56bf4866b728352c53e13711745a3514986873
Thiga <matthewthiga72@gmail.com>
2023-10-16 13:04:05 -0700

    0x05_1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int- Swaps the values of two integers
- * @*a: Pointer to first integer
- * @*b: Pointer to second integer
+ * @a: Pointer to first integer
+ * @b: Pointer to second integer
  * Return: nothing
  */
 
