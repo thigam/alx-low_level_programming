83f52ff43fb34d4fc60b4d8f0ef60267c1af33d4
Thiga <matthewthiga72@gmail.com>
2023-10-19 13:03:45 -0700

    First push for 0x06_9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,44 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Matthew T Mwangi
+ */
+
+#include "main.h"
+
+/**
+ * print_number- Prints an integer
+ * @n: Integer to be printed
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	int counter, holder, digits, output;
+
+	digtis = 1;
+	holder = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar(45);
+	}
+	
+	while (n > 9)
+	{
+		holder /= 10;
+		digits++;
+	}
+
+	for (counter = digits - 1; counter >= 0; counter__)
+	{
+		if ( n == 0)
+		{
+			_putchar(48);
+			break;
+		}
+
+		output = n / (10 ** counter);
+		_putchar(output + '0');
+	}
+}
