bc8983bf5863367faa756cb7614e7d719826ba9c
Thiga <matthewthiga72@gmail.com>
2023-10-19 11:11:43 -0700

    First push for 0x06_7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+/*
+ * File: 7-leet.c
+ * Auth: Matthew T Mwangi
+ */
+
+#include "main.h"
+
+/**
+ * leet- Encodes a string into 1337
+ * @strn: String with the characters to be encoded
+ * Return: A pointer to strn with encoded characters
+ */
+
+char *leet(char *strn)
+{
+	int counter, table_index;
+	char table[5] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	for (counter = 0; strn[counter] != '\0'; counter++)
+	{
+		for (table_index = 0; table[table_index] != '\0'; table_index++)
+		{
+			if (strn[counter] == table[table_index] || strn[counter] - 32 == table[table_index])
+			{
+				strn[counter] = table[table_index] + '0';
+			}
+		}
+	}
+	return (strn);
+}
