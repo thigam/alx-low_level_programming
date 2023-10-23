c59945baac498d0baa5e9798e1de3f0565d7b664
thigam <matthewthiga72@gmail.com>
2023-10-23 12:22:20 -0700

    First push for 0x07_1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy- Copies memory area from one parameter to the other
+ * @dest: The destination: where the memory area
+ * will be copied
+ * @src: The source: where the memory area is copied from
+ * @n: The number of bytes to be copied
+ * Return: A pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int oounter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		dest[counter] = src[counter];
+	}
+
+	return (dest);
+}
