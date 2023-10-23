f27a84995593803817e9eb3e3dcc90cbdddc6d28
thigam <matthewthiga72@gmail.com>
2023-10-23 12:31:46 -0700

    First push for 0x07_2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strchr- Locates a charachter in a string
+ * @s: The string in which the charachter is searched for
+ * @c: The character being searched for
+ * Return: A pointer to the first occurence of the character
+ * or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+		{
+			s++;
+		}
+		else
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+	{
+		return (s)
+	}
+
+	return (NULL);
+}
