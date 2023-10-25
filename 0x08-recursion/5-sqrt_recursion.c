eeb553f665f9dbfd793e507404c62c7ccc73f0ca
thigam <matthewthiga72@gmail.com>
2023-10-25 15:42:27 -0700

    First for 0x08_5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,7 +7,7 @@ int _sqrt_recursion(int n);
  * _sqrt_recursion- Calculates the natural square root of a
  * number
  *
- * @n: The number who's natural square root is to 
+ * @n: The number who's natural square root is to
  * be calculated
  *
  * Return: The natural square root of the number
@@ -16,13 +16,23 @@ int _sqrt_recursion(int n);
 int _sqrt_recursion(int n)
 {
 	int y = 0;
+
 	if (n < 0)
 		return (-1);
 	if (n == 1)
 		return (1);
 
-	return(sqrt_helper(n, y));
+	return (sqrt_helper(n, y));
 }
+
+/**
+ * sqrt_helper- Finds the natural square root of a number
+ *
+ * @x: The number who's square root is being searched for
+ * @root: The number suspected to be the root of x
+ *
+ * Return: -1 if x has no natural root, otherwise the root
+ */
 
 int sqrt_helper(int x, int root)
 {
