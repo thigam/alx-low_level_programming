cf4ce430042faf5d4750830fd8be7525a963cb98
thigam <matthewthiga72@gmail.com>
2023-10-25 07:56:28 -0700

    First push for 0x08_0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- Prints a string followed by a new
+ * line
+ *
+ * @s: Pointer to the string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s[0]);
+
+	if (s[0] != '\0')
+	{
+		_puts_recursion (s++);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+A project on recursion and its applications.
