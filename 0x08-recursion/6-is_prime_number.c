9f53ba0cc0ed77713d24181d403c8e13cebd5276
thigam <matthewthiga72@gmail.com>
2023-10-25 15:47:28 -0700

    First for 0x08_6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+int is_prime_number(int n);
+int divider(int x, int div);
+
 /**
  * is_prime_number- Determines whether a number is a prime
  * number
@@ -11,10 +14,39 @@
 
 int is_prime_number(int n)
 {
-	if (n < 1)
+	int y = 2;
+
+	if (n <= 1)
 	{
-		n *= -1;
+		return (0);
 	}
-	if (n > 1)
+	if (n >= 2 && n <= 3)
 	{
-		if (n
+		return (1);
+	}
+
+	return(divider(n, y));
+}
+
+/**
+ * divider- Divides repeatedly to confirm divisibility
+ *
+ * @x: The number being divided
+ * @div: The divisor
+ *
+ * Return: 0 if x is divisible, -1 if not
+ */
+
+int divider(int x, int div)
+{
+	if (x % div == 0)
+	{
+		return (0);
+	}
+	if (div == x / 2)
+	{
+		return (1);
+	}
+
+	return (divider(x, div + 1));
+}
