04b66c1ea35c1c1622cd347e5664c37b2c2eb398
thigam <matthewthiga72@gmail.com>
2023-10-25 15:28:40 -0700

    Added code for the zero case in 0x08_4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -18,9 +18,14 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	}
 
+	if (y == 1)
+	{
+		return (1);
+	}
+
 	if (y > 0)
 	{
-		x *= _pow_recursion(x, --y);
+		x *= _pow_recursion(x, y - 1);
 	}
 	return (x);
 }
