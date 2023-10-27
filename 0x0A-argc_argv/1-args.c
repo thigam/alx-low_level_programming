a1d95874adbe04dd4721568c49f1e2e58ceb11be
Matthew Mwangi <matthewthiga72@gmail.com>
2023-10-27 08:12:29 -0700

    First push for 0x0A_1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- Prints the number of arguments passed to it
+ *
+ * @argc: The number of arguments passed
+ * @argv: A pointer to an array of pointers to the arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d", argc);
+
+	return (0);
+}
