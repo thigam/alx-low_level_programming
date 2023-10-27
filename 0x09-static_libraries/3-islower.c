524472e7d4a6042e202bc2f32cfa016d510569a8
Matthew Mwangi <matthewthiga72@gmail.com>
2023-10-27 15:39:20 -0700

    updating

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,1 +1,17 @@
-int _islower(int c);
+#include "main.h"
+
+/**
+ * _islower- Tests whether a character is lowercase or not
+ * @c: Integer representing letter
+ * Return: 1 for lowercase or 0 for uppercase and any other character
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
