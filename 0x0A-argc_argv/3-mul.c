dd17dec9e4c80d226c9e0f4903a1ddd57f56047f
Matthew Mwangi <matthewthiga72@gmail.com>
2023-10-27 08:26:43 -0700

    First for 0x0A_3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- multiplies tow integers and prints the result
+ *
+ * @argc: number of command line arguments
+ * @argv: A pointer to an array of pointers to the command
+ * line arguments
+ *
+ * Return: 1 if error, 0 otherwise
+ */
+
+int main(int argc, int *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	return (argv[1] * argv[2]);
+}
