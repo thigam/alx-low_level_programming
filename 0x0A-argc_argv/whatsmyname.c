51c26b3c93b233466469068be74749be484d0757
Matthew Mwangi <matthewthiga72@gmail.com>
2023-10-27 07:56:44 -0700

    First for 0x0A_0

---
+++ 0x0A-argc_argv/whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * whatsmyname- Prints its own name
+ *
+ * @argc: Commmand line argument counter
+ * @argv: An array of pointers to the command line arguments
+ *
+ * Return: 0
+ */
+
+int main(argc, *argv[])
+{
+	printf(argv[0]);
+	return (0);
+}
