14dec9afabdf0e37e4a7784a9212ccdb626d39ae
Matthew Mwangi <matthewthiga72@gmail.com>
2023-10-30 04:45:41 -0700

    First push for 0x0B_2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * str_concat- Concatenates two strings
+ *
+ * @s1: First string
+ * @s2: second string
+ *
+ * Return: NULL on failure, poiner to a newly allocated space
+ * containing the contents of s1 followed by the contents of s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat_str;
+	int counter_s1, counter_s2, len = 0;
+
+	for (counter_s1 = 0; s1[counter_s1]; counter_s1++)
+	{
+		len++;
+	}
+
+	for (counter_s2; s2[counter_s2]; counter_s2++)
+	{
+		len++;
+	}
+
+	len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	for (counter_s1 = 0; s1[counter_s1]; counter_s1++)
+	{
+		concat_str[counter_s1] = s1[counter_s1];
+	}
+
+	for (counter_s2 = 0; s2[counter_s2]; counter_s2++)
+	{
+		concat_str[counter_s1 + counter_s2] = s2[counter_s2];
+	}
+
+	concat_str[len] = '\0';
+
+	return (concat_str);
+}
