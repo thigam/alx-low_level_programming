5fbac893c0a7cf1ddb03df52206551fc9db8275f
Matthew Mwangi <matthewthiga72@gmail.com>
2023-10-30 04:28:23 -0700

    First push for 0x0B_1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup- Returns a pointer to a new string which
+ * is a duplicate of the string str
+ *
+ * @str: The string to be duplicated
+ *
+ * Return: NULL is str == NULL or if insufficient memory
+ * was available, otherwise returns pointer to the duplicated
+ * string
+ */
+
+char *_strdup(char *str)
+{
+	char *new_string;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_string = malloc(len(*str));
+
+	if (new_string == NULL)
+		return (NULL);
+
+	return (new_string);
+}
