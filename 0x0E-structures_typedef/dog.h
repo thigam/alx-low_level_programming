c0203d4c4e89143a305a358707e1143ae95c8706
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-06 09:12:56 -0800

    oxoE_1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - A type for dogs description
  * @name: It's name
@@ -19,3 +22,4 @@ struct dog
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
