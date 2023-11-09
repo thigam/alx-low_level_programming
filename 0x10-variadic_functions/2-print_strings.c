30f2d3ddbbb47f61d3ef1676693f533943b84964
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-09 07:13:39 -0800

    First push

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings- Prints stirngs
+ * @separator: The string to be printed between strings
+ * @n: The umber of strings
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list words;
+	unsigned int counter;
+
+	va_start(words, n);
+
+	if (separator == NULL)
+		return;
+	for (counter = 1; counter <= n; counter++)
+	{
+		printf("%s", va_arg(words, char *));
+		if (counter != n)
+			printf("%s", separator);
+	}
+	printf("\n");
+	return;
+}
