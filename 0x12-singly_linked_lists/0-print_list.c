e8ef3135d1bdeb67948a86511cf754acf386c9b5
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-16 08:16:26 -0800

    Used -> for the pointer h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,14 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h.str == NULL)
-			printf("[0] (nil)");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
 		{
-			printf("[%d] %s\n", h.len, h.str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 
-		h = h.next;
+		h = h->next;
 		nod++;
 	}
 	return (nod);
