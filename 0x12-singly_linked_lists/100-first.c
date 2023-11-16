4cda40b9c5a8e31312891794be9e1c054e189955
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-16 14:31:29 -0800

    Changed prototyppe during initialization

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Return: nothing
  */
 
-void beginner (void) __attribute__ ((constructor))
+void beginner (void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
