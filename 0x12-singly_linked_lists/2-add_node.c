e5d4b072829eabbac627be50a406bfe6b2ef7591
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-16 08:44:21 -0800

    first push

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_node- Adds a new node at the begining of a list_t list
+ * @head: A pointer to the list_t
+ * @str: The string to be added to the list_t list
+ *
+ * Return: address of the new element or NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *another_one;
+
+	another_one = malloc(sizeof(list_t));
+
+	if (another_one == NULL)
+		return (NULL);
+	another_one->str = strdup(head->str);
+	another_one->len = strlen(head->str);
+	another_one->next = head;
+	return (another_one);
+}
