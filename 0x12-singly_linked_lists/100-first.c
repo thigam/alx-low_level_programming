90156468a2413379e1a3a8019ad8867065704c3a
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-16 14:28:05 -0800

    First push

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * beginner- Prints a line before main
+ *
+ * Return: nothing
+ */
+
+void beginner (void) __attribute__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
