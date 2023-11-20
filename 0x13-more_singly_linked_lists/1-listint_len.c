56a89c84eeacbbdd2fb4bee50adad4cc4a29082d
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-20 10:32:15 -0800

    First push

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len- Returns the number of elements in a linked listint_t list
+ * @h: The pointer to the first node
+ * 
+ * Return: The number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 1;
+	listint_t *holder = h;
+
+	while (holder->next)
+	{
+		holder = holder->next;
+		counter++;
+	}
+
+	return (counter);
+}
