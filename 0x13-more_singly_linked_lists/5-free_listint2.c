f57da8c53a3208c53039e68d813da6900af34b58
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-20 11:15:56 -0800

    First push

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2- frees a listint_t list
+ * @head: a pointer to a pointer to the first node
+ *
+ * Reutrn: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *holder;
+
+	holder = *head;
+	head = NULL;
+
+	if (holder->next != NULL)
+		free_listint2(holder->next);
+
+	free(holder);
+}
