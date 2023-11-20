1792a9fd13eda55a207af21e477e55fdcb010e80
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-20 12:29:31 -0800

    Corrected misspelled variable

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (holder == NULL)
 			return (-1);
 
-		if (counter == next)
+		if (counter == index)
 		{
 			holder_2->next = holder->next;
 			free(holder);
