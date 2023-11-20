205fd5389b5493d9e6ce33eba3d48b6ba3740c9d
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-20 11:36:13 -0800

    First push

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index- Returns the nth node of a listint_t linked list
+ * @head: A pointer to the first node
+ * @index: The position of the node
+ *
+ * Return: A pointer to the node requested, NULL if node doesn't exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *holder;
+	unsigned int counter;
+
+	holder = head;
+	counter = 0;
+
+	if (counter > index)
+		return (NULL);
+
+	for (; counter <= index; counter++)
+		holder = holder->next;
+
+	if (holder == NULL)
+		return (NULL);
+
+	return (holder);
+}
