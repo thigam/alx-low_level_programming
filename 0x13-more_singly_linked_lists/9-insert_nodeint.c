8b07a312ddc5361e4d612e36cff99a63df2a1275
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-20 12:05:56 -0800

    First push

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index- Inserts a new node at a given position
+ * @head: A pointer to a pointer to the first node
+ * @idx: Index where the new node should be added
+ * @n: Integer contained in the new node
+ *
+ * Return: address of the new node, NULL if failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int counter;
+	listint_t *holder;
+	listint_t *another;
+
+	holder = *head;
+	counter = 0;
+
+	if (index == 0)
+	{
+		another = malloc(sizeof(listint_t));
+		if (another == NULL)
+			return (NULL);
+		another->n = n;
+		another->next = holder->next;
+		*head = another;
+		return (another);
+	}
+
+	index--;
+
+
+	while (counter <= index)
+	{
+		if (holder == NULL)
+			return (NULL);
+		if (counter == index)
+		{
+			another  = malloc(sizeof(listint_t));
+			if (another == NULL)
+				return (NULL);
+			another->n = n;
+			another->next = holder->next;
+			holder->next = another;
+			return (another);
+		}
+		counter++
+		holder = holder->next;
+	}
+}
