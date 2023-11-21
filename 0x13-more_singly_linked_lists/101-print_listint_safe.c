dfc8e689374bc73eb586f4ef49a73b9aab2c8cff
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-21 07:29:35 -0800

    First push

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe- prints a listint_t linked list
+ * @head: a pointer to the first node
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *holder;
+	size_t counter;
+
+	holder = head;
+	counter = 0;
+
+	if (holder == NULL)
+		exit(98);
+
+	while (holder)
+	{
+		printf("[%p] %d", holder, holder->n);
+		holder = holder->next;
+		counter++;
+	}
+	return (counter);
+}
