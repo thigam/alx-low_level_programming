ca573227127fdc923a23817d5d58157c0740cf95
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-23 12:48:44 -0800

    Added reverser

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,43 @@
 
 void print_binary(unsigned long int n)
 {
-	while (n)
+	char holder[64] = {'\0'};
+
+	if (n == 0)
 	{
-		if ((n & 1) == 1)
-			printf("%d", 1);
-		else if ((n & 1) == 0)
-			printf("%d", 0);
-		n = n>>1;
+		_adder(holder, '0');
 	}
+	else
+	{
+		while (n)
+		{
+			if ((n & 1) == 1)
+				_adder(holder, '1');
+			else if ((n & 1) == 0)
+				_adder(holder, '0');
+			n = n>>1;
+		}
+	}
+	reverser(holder);
+	printf("%s\n", holder);
+}
+
+void _adder(char *holder, char c)
+{
+	int counter = 0;
+
+	while (holder[counter])
+		counter++;
+
+	holder[counter] = c;
+}
+
+void reverser(char *holder)
+{
+	int index = 0;
+	char temporary;
+
+	temporary = holder[index];
+	holder[index] = holder[strlen(holder) - index - 1];
+	holder[strlen(holder) - index - 1] = temporary;
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,4 +10,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 unsigned int power(int a, int b);
+void _adder(char *holder, char c);
+void reverser(char *holder);
 #endif
