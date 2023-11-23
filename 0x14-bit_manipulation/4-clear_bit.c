0fefea881fab0a8990e584a96841b5351cf994c6
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-23 13:21:43 -0800

    Changed logic

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,72 +10,12 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int *left;
-	unsigned long int *right;
+	unsigned long int holder;
 
-	if (index < 0)
-		return (-1);
+	holder = 1;
+	holder = holder<<index;
 
-	left = left_half(*n, index);
-	right = right_half(*n, index);
-
-	*n = left | right;
+	*n = *n & (~holder);
 
 	return (1);
-}
-
-unsigned long int *right_half(unsigned long int *n, unsigned int index)
-{
-	unsigned int counter, counter_holder;
-
-	counter = 0;
-
-	while (*n)
-	{
-		n = n>>1;
-		counter++;
-	}
-
-	counter = counter - index;
-	counter_holder = counter;
-
-	while (counter > 0)
-	{
-		*n = *n<<1;
-		counter--;
-	}
-
-	counter = counter_holder;
-
-	while (counter > 0)
-	{
-		*n = *n>>1;
-		counter--;
-	}
-
-	return (n);
-}
-
-unsigned long int *left_half(unsigned long int *n, unsigned int index)
-{
-	unsigned int index_holder;
-
-	index++;
-	index_holder = index;
-
-	while (index > 0)
-	{
-		*n = *n>>1;
-		index--;
-	}
-
-	index = index_holder;
-
-	while (index > 0)
-	{
-		*n = *n<<1;
-		counter--;
-	}
-
-	return (n);
 }
