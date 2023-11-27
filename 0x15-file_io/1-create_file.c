a84edc600647e1bca19f0eb284f349c6ebecc922
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-27 14:35:47 -0800

    Closed file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,11 @@ int create_file(const char *filename, char *text_content)
 
 	write_return = write(fd, text_content, sizeof(text_content)/sizeof(char *));
 	if (write_return == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
+	close(fd);
 	return (1);
 }
