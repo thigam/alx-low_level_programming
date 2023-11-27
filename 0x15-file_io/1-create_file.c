c33a6e7a787501ebbee91ec01b86b2c2b36b8c6e
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-27 15:56:05 -0800

    Corrected open flags

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT | O_EXCL | S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 
