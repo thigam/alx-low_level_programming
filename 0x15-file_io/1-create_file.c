1f35fe5c000e491cd4fee5d57b6f586e5c9c6640
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-27 15:47:09 -0800

    Corrected betty errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	write_return = write(fd, text_content, sizeof(text_content)/sizeof(char *));
+	write_return = write(fd, text_content, sizeof(text_content) / sizeof(char *));
 	if (write_return == -1)
 	{
 		close(fd);
