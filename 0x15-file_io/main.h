64ee2bdcba515fedc4ea6e14bb44ff61367e7e94
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-27 14:10:56 -0800

    Corrected header name to fcntl.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN
 #include <unistd.h>
 #include <stdlib.h>
-#include <fcnt1.h>
+#include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
