5e6765e7f7fd2d40c829b1e9e85586590656ff7a
Matthew Mwangi <matthewthiga72@gmail.com>
2023-11-27 16:02:15 -0800

    Corrected open flags

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,8 @@
  * @letters: the number of letters to be read
  *
  * Return: 0 if file cannot be opened or read,
- * if filename is NULL or if write fails or does not write the expectes number of byte.
+ * if filename is NULL or if write fails or does
+ * not write the expectes number of byte.
  * Otherwise, returns the actual number of letters it could read and print.
  */
 
@@ -19,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, letters);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
@@ -28,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (buff == NULL)
 	{
-		close (fd);
+		close(fd);
 		return (0);
 	}
 
@@ -46,7 +47,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buff);
 	close(fd);
 
-	if (numb_write == -1)
+	if (numb_write == -1 || (size_t)numb_write != (size_t)numb_read)
 		return (0);
 
 	return (numb_write);
