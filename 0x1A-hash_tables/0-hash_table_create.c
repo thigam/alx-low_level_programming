e3f401e4d179491f6b4a7371729073ef13fd6869
Matthew Thiga <matthewthiga72@gmail.com>
2024-02-01 00:00:25 -0800

    Finished on 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,10 +10,27 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i = 0;
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	table->size = size;
 
 	for (i = 0; i < size; i++)
 	{
-		table[i] = malloc(sizeof(hash_table_t));
+		table->array[i] = NULL;
 	}
 
 	return (table);
